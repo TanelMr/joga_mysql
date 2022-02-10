7b00a594844e1d6ac3e74f84b9ebad5dad39e929
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-10 21:44:39 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //define packages
+const path = require('path');
 const express = require ('express');
 const app = express ();
 const mysql = require('mysql');
-
-
-const path = require("path");
 const hbs = require('express-handlebars');
-//add views directory path
+
 app.set('views', path.join(__dirname, 'views'));
 //add views template engine
-app.set('view engine', hbs.engine({
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir:__dirname + '/views/layouts'
+    layoutsDir:__dirname + '/views/layouts',
 }));
+
 //setup static public directory
 app.use(express.static('public'));
 
@@ -34,7 +34,19 @@ con.connect(function(err) {
     console.log("Connected to database joga_mysql");
 });
 
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
+
 //listen on port 3000
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Server started');
 });
